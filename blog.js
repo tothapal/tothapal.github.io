8fc9a040bade494e7a0a298b89405e2f252b0043
tothapal <tothapal@gmail.com>
2019-02-23 11:53:38 +0100

    .

---
+++ blog.js
@@ -0,0 +1,29 @@
+$('posttext').html('');
+
+$('button').on('click',(event) =>{
+    event.preventDefault();
+    var d = new Date();
+    var ujdate = d.toLocaleDateString("hu-HU",{year: "numeric",month:"2-digit",day:"2-digit",hour:"2-digit",minute:"2-digit"})
+    let cim = $('#title').val();
+    $('#title').val("");
+    let szoveg = $('#posttext').val();  
+    $('#posttext').val(""); 
+
+    database = firebase.database();
+
+    let pathtoPosts = "postok";
+    let data = {title: cim, text: szoveg, datum: ujdate};
+
+    database.ref(pathtoPosts).push(data);
+});
+
+database.ref("postok").once('value').then(data=>{
+
+    data.forEach(element => {
+        $('#oldposts').append(`
+            <h3>${element.child('title').val()}</h3>
+            <h4>${element.child('datum').val()}</h4>
+            <div>${element.child('text').val()}</div>
+        `);
+    });
+})
